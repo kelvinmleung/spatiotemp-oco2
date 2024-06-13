d053f3c5d0ec9acce34c9fc909b4ec05406f5955
Camila Moran-Hidalgo <cmohi@mit.edu>
2024-06-13 12:17:46 -0400

    wrote forward model and necessary variables to npy files

--- extract_model.py
+++ extract_model.py
@@ -4,6 +4,10 @@ import h5py
 filepath = '/Users/Camila/Desktop/OCO-2 UROP/spatiotemp-oco2/linear_oco_model/example_oco2_linear_model_201510_lamont.h5'
 with h5py.File(filepath, 'r') as f:
     Fmatrix = f['model_matrix'][:,:]
+    true_x = f['state_true_mean_vector'][:]
+    prior_mean_x = f['operational_prior_mean_vector'][:]
+    prior_cov_x = f['operational_prior_covariance_matrix'][:,:]
+    error_variance = f['error_variance_diagonal'][:]
 
     # Print the dimensions of each key
     for key in f.keys():
@@ -13,3 +17,7 @@ with h5py.File(filepath, 'r') as f:
     print(f['state_vector_names'][:])
 
 # np.save('example_oco2_linear_model_201706_wollongong.npy', Fmatrix)
+np.save('true_state_vector_2015-10_lamont.npy', true_x)
+np.save('prior_mean_2015-10_lamont.npy', prior_mean_x)
+np.save('prior_cov_matrix_2015-10_lamont.npy', prior_cov_x)
+np.save('error_variance_2015-10_lamont.npy', error_variance)
